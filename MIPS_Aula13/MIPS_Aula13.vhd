7e8e6274cf33d5110d544fe5f3481e06965bfe7c
AntonioFuziy <antoniovf@al.insper.edu.br>
2021-10-19 13:41:43 -0300

    feat: creafirst commit on branch Fuziy

--- MIPS_Aula13/MIPS_Aula13.vhd
+++ MIPS_Aula13/MIPS_Aula13.vhd
@@ -5,6 +5,7 @@ entity MIPS_Aula13 is
   -- Total de bits das entradas e saidas
   generic ( 
 	larguraDados : natural := 32;
+	larguraAddrRegistradores : natural := 5;
 	simulacao : boolean := FALSE -- para gravar na placa, altere de TRUE para FALSE
   );
   port   (
@@ -13,7 +14,7 @@ entity MIPS_Aula13 is
 	 SW: in std_logic_vector(9 downto 0);
 	 FPGA_RESET_N: in std_logic;
 	 LEDR: out std_logic_vector(9 downto 0);
-	 Teste_Endereco: out std_logic_vector(8 downto 0)
+	 Teste_Endereco: out std_logic_vector(larguraDados-1 downto 0)
 --	 HEX0 : out std_logic_vector(6 downto 0);
 --	 HEX1 : out std_logic_vector(6 downto 0);
 --	 HEX2 : out std_logic_vector(6 downto 0);
@@ -27,58 +28,55 @@ end entity;
 architecture arquitetura of MIPS_Aula13 is
 
 -- Faltam alguns sinais:
-	signal instruction: std_logic_vector(14 downto 0);
 	signal CLK : std_logic;
 	signal PC_ROM : std_logic(larguraDados-1 downto 0);
 	
-	signal SaidaREG_LEDR: std_logic_vector(9 downto 0);
+--	signal SaidaREG_LEDR: std_logic_vector(9 downto 0);
+	signal somador_PC: std_logic_vector(larguraDados-1 downto 0);
+	
+	signal bancoReg_ULA_A: std_logic_vector(larguraDados-1 downto 0);
+	signal bancoReg_ULA_B: std_logic_vector(larguraDados-1 downto 0);
+	signal saida_ULA: std_logic_vector(larguraDados-1 downto 0);
+	
+	constant instruction: std_logic_vector(larguraDados-1 downto 0) := "32bits";
+	constant Operacao_ULA: std_logic_vector(1 downto 0) := "2bits";
+	constant Wr_R3: std_logic := '1bit';
 
 begin
 
 -- Instanciando os componentes:
 
-	CLK <= CLOCK_50;
+CLK <= CLOCK_50;
 	
 PC : entity work.registradorGenerico   generic map (larguraDados => larguraDados)
-          port map (DIN => , DOUT => PC_ROM, ENABLE => '1', CLK => CLK, RST => '0');
+          port map (DIN => somador_PC, DOUT => PC_ROM, ENABLE => '1', CLK => CLK, RST => '0');
 			 
 somaUm :  entity work.somaConstante  generic map (larguraDados => larguraDados, constante => 4)
-        port map( entrada => PC_ROM, saida => );
-
---	detectorSub0: work.edgeDetector(bordaSubida)
---			  port map (clk => CLOCK_50, entrada => (not KEY(0)), saida => CLK_KEY0);
---			  
---	detectorSub1: work.edgeDetector(bordaSubida)
---			  port map (clk => CLOCK_50, entrada => (not KEY(1)), saida => CLK_KEY1);
-
+        port map( entrada => PC_ROM, saida => somador_PC);
 
 ULA1 : entity work.ULASomaSub  generic map(larguraDados => larguraDados)
-          port map (entradaA => , entradaB => , saida => Saida_ULA, seletor => Operacao_ULA);		
+          port map (entradaA => bancoReg_ULA_A, entradaB => bancoReg_ULA_B, saida => Saida_ULA, seletor => Operacao_ULA);		
 			 
-Banco_Registradores : entity work.bancoRegistradores   generic map (larguraDados => larguraDados, larguraEndBancoRegs => 2)
-		 port map ( 
+Banco_Registradores : entity work.bancoRegistradores   
+		generic map (larguraDados => larguraDados, larguraEndBancoRegs => larguraAddrRegistradores)
+		port map ( 
 			  clk => CLK,
-			  endereco => Instruction_IN(10 downto 9),
-			  dadoEscrita => Saida_ULA,
-			  habilitaEscrita => Habilita_A,
-			  saida  => REG1_ULA_A
+			  enderecoA => instruction(15 downto 11),    
+			  enderecoB => instruction(20 downto 16),   
+			  enderecoC => instruction(25 downto 21),
+			  dadoEscritaC => saida_ULA,
+			  escreveC => Wr_R3,
+			  saidaA => bancoReg_ULA_A,
+			  saidaB => bancoReg_ULA_B
 		 );
 		 
 ROM1 : entity work.ROM   generic map (dataWidth => larguraDadosROM, addrWidth => larguraAddrROM)
 		 port map (
-			 Endereco => Addr_ROM,
+			 Endereco => PC_ROM,
 			 Dado => instruction
 		 );
-				 
---memoriaDados : entity work.memoriaDados   generic map (dataWidth => 8, addrWidth => 6)
---		 port map (addr => Data_Address(5 downto 0), we => wr, re => rd, habilita => Saida_Decoder_Blocos(0), dado_in => Data_OUT, dado_out => Data_IN, clk => CLK);
-		 
--- ================================================= HEX 0 ====================================================
-		
 
--- ================================================= Saidas e Operacoes ====================================================
-
-LEDR <= SaidaREG_LEDR;
+--LEDR <= SaidaREG_LEDR;
 
 Teste_Endereco <= Addr_ROM;
 
