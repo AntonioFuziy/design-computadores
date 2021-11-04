a584ca732180eb6eb344d2cadce7682a67a47bc5
AntonioFuziy <antoniovf@al.insper.edu.br>
2021-11-04 15:57:40 -0300

    feat: ULA aula 16

---
+++ MIPS/MIPS_Aula16_ULA/somadorGenerico.vhd
@@ -0,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity somadorGenerico is
+    port
+    (
+        entradaA, entradaB: in std_logic;
+        carryIn: in std_logic;
+        carryOut: out std_logic;
+        saida:  out std_logic
+    );
+end entity;
+
+architecture comportamento of somadorGenerico is
+    begin
+        saida <= carryIn xor (entradaA xor entradaB);
+        carryOut <= (entradaA and entradaB) or (carryIn and (entradaA xor entradaB));
+end architecture;
